19e46696ec93a6a593ec7a427768d9a6eaead80a
arissi <anthonyxrissi@gmail.com>
2017-04-21 21:30:29 -0500

    fix path to image

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
     consumerKey : 'i4SLOAvqfp4w0l2wdogIcqRzB',
     consumerSecret : 'BaJ50c273HVH2WcDo0glc4V8awpawKBNqRRXCcSDUXswcBOnPh',
-    callbackURL : 'http://127.0.0.1:3000/auth/twitter/callback'
+    callbackURL : 'https://tigerhopping.herokuapp.com/auth/twitter/callback'
 }
